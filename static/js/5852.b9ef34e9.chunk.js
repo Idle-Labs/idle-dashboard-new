15a15ab09968b48041efd4aaa66da7fe6627b68f
Samster91 <samster91>
2022-12-20 18:12:34 +0100

    Updates

---
+++ static/js/5852.b9ef34e9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkidle_dashboard_new=self.webpackChunkidle_dashboard_new||[]).push([[5852],{85852:function(e,a,d){d.r(a),a.default={}}}]);
+//# sourceMappingURL=5852.b9ef34e9.chunk.js.map
---
+++ static/js/7972.6849f2f9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkidle_dashboard_new=self.webpackChunkidle_dashboard_new||[]).push([[7972],{77972:function(e,a,d){d.r(a),a.default={}}}]);
+//# sourceMappingURL=7972.6849f2f9.chunk.js.map
