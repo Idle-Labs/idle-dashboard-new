321151b3f2ca8ad9124550775fcee9a097507703
Samster91 <samster91>
2023-08-03 17:51:23 +0200

    FIX: polygon zkevm rpc

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -25,8 +25,9 @@ export const chains: Record<number, Chain> = {
     id: '0x44d',
     token: 'ETH',
     label: 'Polygon zkEVM',
+    rpcUrl: `https://zkevm-rpc.com`,
     publicRpcUrl: `https://zkevm-rpc.com`,
-    rpcUrl: `https://polygonzkevm-mainnet.g.alchemy.com/v2/${env.REACT_APP_ALCHEMY_ZK_KEY}`
+    // rpcUrl: `https://polygonzkevm-mainnet.g.alchemy.com/v2/${env.REACT_APP_ALCHEMY_ZK_KEY}`
   }
 }
 
