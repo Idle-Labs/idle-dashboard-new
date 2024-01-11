cdef82809a524ff5ea352a2f32d6dfe7fed70ac5
Samster91 <samster91>
2024-01-11 17:45:04 +0100

    FIX: metamorpho vaults description

--- src/constants/vaults.ts
+++ src/constants/vaults.ts
@@ -2869,7 +2869,7 @@ export const tranches: Record<number, Record<string, Record<string, TrancheConfi
           abi:MorphoPool as Abi,
           address:'0x38989BBA00BDF8181F4082995b3DEAe96163aC5D'
         },
-        description:'This strategy deploys funds in the <a href="https://app.morpho.org/vault?vault=0x38989BBA00BDF8181F4082995b3DEAe96163aC5D" class="link" rel="nofollow noopener noreferrer" target="_blank">BlockAnalitica WETH MetaMorpho vault</a>. The Flagship ETH MetaMorpho vault curated by B.Protocol and Block Analitica is intended to optimize risk-adjusted interest earned from blue-chip LST and stablecoin collateral markets. The APR is dynamically adjusted according to the coverage provided to the counterpart Senior tranche thanks to the <a href="https://medium.com/idle-finance/adaptive-yield-split-foster-pyts-liquidity-scalability-a796fa17ea35" class="link" rel="nofollow noopener noreferrer" target="_blank">Adaptive Yield Split</a>.',
+        description:'This strategy deploys funds in the <a href="https://app.morpho.org/vault?vault=0x38989BBA00BDF8181F4082995b3DEAe96163aC5D" class="link" rel="nofollow noopener noreferrer" target="_blank">Flagship ETH MetaMorpho vault</a>. The vault curated by B.Protocol and Block Analitica is intended to optimize risk-adjusted interest earned from blue-chip LST and stablecoin collateral markets. The APR is dynamically adjusted according to the coverage provided to the counterpart Senior tranche thanks to the <a href="https://medium.com/idle-finance/adaptive-yield-split-foster-pyts-liquidity-scalability-a796fa17ea35" class="link" rel="nofollow noopener noreferrer" target="_blank">Adaptive Yield Split</a>.',
         Tranches:{
           AA:{
             abi:ERC20 as Abi,
