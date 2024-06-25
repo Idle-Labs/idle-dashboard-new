71750b0c5b28e243e4accc949ba0f97ca2b2b712
Samster91 <samster91>
2024-06-25 17:02:59 +0200

    FIX: subgraphs migration

--- src/constants/subgraphs.ts
+++ src/constants/subgraphs.ts
@@ -1,28 +1,30 @@
 type SubgraphProps = {
-  enabled: boolean
-  endpoints: Record<number, string>
-  entities: Record<string, string[]>
-}
+  enabled: boolean;
+  endpoints: Record<number, string>;
+  entities: Record<string, string[]>;
+};
+
+const env = process.env;
 
 export const subgraphs: Record<string, SubgraphProps> = {
   tranches: {
-    enabled:true,
-    endpoints:{
-      1:"https://api.thegraph.com/subgraphs/name/samster91/idle-tranches",
-      10:"https://api.thegraph.com/subgraphs/name/samster91/idle-tranches-optimism",
-      1101:"https://api.studio.thegraph.com/query/12583/idle-tranches-zkevm/version/latest"
+    enabled: true,
+    endpoints: {
+      1: `https://gateway-arbitrum.network.thegraph.com/api/${env.REACT_APP_SUBGRAPH_KEY}/subgraphs/id/C29JvonxZXzgmxi57g3vznAdxyeDozghz2twmwUPE6oG`,
+      10: "https://api.studio.thegraph.com/query/12583/idle-tranches-optimism/version/latest",
+      1101: "https://api.studio.thegraph.com/query/12583/idle-tranches-zkevm/version/latest",
     },
-    entities:{
-      trancheInfos:[
-        'id',
-        'apr',
-        'timeStamp',
+    entities: {
+      trancheInfos: [
+        "id",
+        "apr",
+        "timeStamp",
         // 'blockNumber',
         // 'totalSupply',
-        'virtualPrice',
-        'contractValue',
-      ]
-    }
+        "virtualPrice",
+        "contractValue",
+      ],
+    },
   },
   /*
   chainlinkFeeds: {
@@ -42,4 +44,4 @@ export const subgraphs: Record<string, SubgraphProps> = {
     }
   }
   */
-}
+};
