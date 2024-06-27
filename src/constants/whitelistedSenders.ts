6fcc90595a52de9e4a7780cf8c5718884ae01246
Samster91 <samster91>
2024-06-27 17:16:11 +0200

    FIX: exclude RWA rewards from tranches

--- src/constants/whitelistedSenders.ts
+++ src/constants/whitelistedSenders.ts
@@ -1,28 +1,35 @@
-import type { RewardSenders } from 'constants/types'
-import { IDLE_MULTISIG_MAINNET, IDLE_MULTISIG_OPTIMISM, DISPERSE_APP_OPTIMISM, DISPERSE_APP_MAINNET } from 'constants/addresses'
+import type { RewardSenders } from "constants/types";
+import {
+  IDLE_MULTISIG_MAINNET,
+  IDLE_MULTISIG_OPTIMISM,
+  DISPERSE_APP_OPTIMISM,
+  DISPERSE_APP_MAINNET,
+} from "constants/addresses";
 
 export const rewardsSenders: Record<number, RewardSenders> = {
   10: {
-    [DISPERSE_APP_OPTIMISM]: {},
-    [IDLE_MULTISIG_OPTIMISM]: {}
-  }
-}
+    [DISPERSE_APP_OPTIMISM]: {
+      endBlock: 119519916,
+    },
+    [IDLE_MULTISIG_OPTIMISM]: {},
+  },
+};
 
 export const rewardsSendersRwa: Record<number, RewardSenders> = {
   10: {
     [DISPERSE_APP_OPTIMISM]: {
-      startBlock: 119519917
+      startBlock: 119519917,
     },
     [IDLE_MULTISIG_OPTIMISM]: {
-      startBlock: 119519916,
-      endBlock: 119519916
-    }
-  }
-}
+      // startBlock: 119519916,
+      endBlock: 119519916,
+    },
+  },
+};
 
 export const distributedFeesSenders: Record<number, RewardSenders> = {
   1: {
-    [IDLE_MULTISIG_MAINNET]:{},
-    [DISPERSE_APP_MAINNET]:{}
-  }
-}
+    [IDLE_MULTISIG_MAINNET]: {},
+    [DISPERSE_APP_MAINNET]: {},
+  },
+};
